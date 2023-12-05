14ab50f793366b42a00fa33eaafe613bf9ec8503
Pr0ximah <2291634021@qq.com>
2023-12-05 12:50:44 +0800

    feat: server version v1.1 fix: Wallpaper auto change fix: Search result show

--- vite.config.js
+++ vite.config.js
@@ -12,14 +12,16 @@ export default defineConfig({
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+      '~@': fileURLToPath(new URL('./src', import.meta.url)),
     }
   },
   build: {
+    emptyOutDir: true,
     rollupOptions: {
       input: {
-        main: resolve(__dirname, 'index.html'),
-        search: resolve(__dirname, 'pages/search.html'),
+        index: resolve(__dirname, '/index.html'),
+        search: resolve(__dirname, '/search.html'),
       }
     }
   },
