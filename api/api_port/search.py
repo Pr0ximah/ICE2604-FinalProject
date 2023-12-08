ef8ce45d5e814a00150294f41ece701fc8707c66
Pr0ximah <2291634021@qq.com>
2023-12-08 15:10:16 +0800

    fix: search bug

--- api/api_port/search.py
+++ api/api_port/search.py
@@ -1,46 +1,49 @@
 import json
 from elasticsearch import Elasticsearch
+
 # import db_tool
 
-es = Elasticsearch()
+# es = Elasticsearch()
+es = Elasticsearch(["http://localhost:9200"])
+
 
 def get_json(string):
-    return json.dumps(string, sort_keys=True, indent=4, separators=(',', ':'))
+    return json.dumps(string, sort_keys=True, indent=4, separators=(",", ":"))
+
 
 def json_print(string):
-    print(json.dumps(string, sort_keys=True, indent=4, separators=(',', ':')))
+    print(json.dumps(string, sort_keys=True, indent=4, separators=(",", ":")))
     return
 
-with open('api/api_port/MetaData.json','r',encoding='utf8') as fp:
+
+with open("api/api_port/MetaData.json", "r", encoding="utf8") as fp:
     json_data = json.load(fp)
 
-mappings = {
-    "properties": {
-        "data": {
-          "type": "text",  
-        },
-        "paper_id":{
-            "type": "text",
-        },
-        "title": {
-          "type": "completion",
-        },
-        "year" : {
-          "type" : "long",
-        },
-        "authors" :{
-            "type" : "list",
-        },
-        "keywords" : {
-            "type" : "list",
-        },
-        "link" : {
-            "type" : "text"
-        },
-        "first_page" : {
-            "type" : "long"
+body = {
+    "mappings": {
+        "properties": {
+            "data": {
+                "type": "text",
+            },
+            "paper_id": {
+                "type": "text",
+            },
+            "title": {
+                "type": "completion",
+            },
+            "year": {
+                "type": "long",
+            },
+            "authors": {
+                "type": "list",
+            },
+            "keywords": {
+                "type": "list",
+            },
+            "link": {"type": "text"},
+            "first_page": {"type": "long"},
         }
-      }
+    }
 }
 
 date = []
@@ -62,92 +65,74 @@ for value in json_data.values():
     link.append(value["link"])
     first_page.append(value["first_page"])
 
-es.indices.delete(index = 'mydatabase')
-es.indices.create(index ='mydatabase',body =mappings,ignore=400)
+# es.indices.delete(index="mydatabase")
+es.indices.create(index="mydatabase", body=body, ignore=400)
 # es.indices.create(index='mydatabase', body=mappings, ignore=400, headers={'Content-Type': 'application/json'}
 for i in range(100):
-    mapping = {"date":date[i],"paper_id":paper_id[i],"title":title[i],"year":year[i],"authors":authors[i],"keywords":keywords[i],"link":link[i],"first_page":first_page[i]}
-    res = es.index(index="mydatabase",id=i,body=mapping)
+    mapping = {
+        "date": date[i],
+        "paper_id": paper_id[i],
+        "title": title[i],
+        "year": year[i],
+        "authors": authors[i],
+        "keywords": keywords[i],
+        "link": link[i],
+        "first_page": first_page[i],
+    }
+    res = es.index(index="mydatabase", id=i, body=mapping)
+
 
 def search_title(str):
     query = {
-        "query":{
-            "wildcard":{
-                "title": "*" + str + "*"
-            }
+        "query": {
+            "wildcard": {"title": "*" + str + "*"}
             # "match":{
             #     "title": str
             # }
-        }
-        ,"sort": [
-            {
-                "_score":{
-                    "order": "desc"
-                }
-            }
-        ]
-        ,"size":1000
+        },
+        "sort": [{"_score": {"order": "desc"}}],
+        "size": 1000,
     }
-    result = es.search(index="mydatabase",body=query)
+    result = es.search(index="mydatabase", body=query)
     hit = result["hits"]["hits"]
     return hit
 
+
 def search_year(time):
-    query = {
-        "query":{
-            "match":{
-                "year": time
-            }
-        }
-        ,"size":1000
-    }
-    result = es.search(index="mydatabase",body=query)
+    query = {"query": {"match": {"year": time}}, "size": 1000}
+    result = es.search(index="mydatabase", body=query)
     hit = result["hits"]["hits"]
     return hit
+
 
 def search_author(str):
     query = {
-        "query":{
-            "wildcard":{
-                "keywords": "*" + str + "*"
-            }
+        "query": {
+            "wildcard": {"keywords": "*" + str + "*"}
             # "match":{
             #     "authors": str
             # }
-        }
-        ,"sort": [
-            {
-                "_score":{
-                    "order": "desc"
-                }
-            }
-        ]
-        ,"size":1000
+        },
+        "sort": [{"_score": {"order": "desc"}}],
+        "size": 1000,
     }
-    result = es.search(index="mydatabase",body=query)
+    result = es.search(index="mydatabase", body=query)
     hit = result["hits"]["hits"]
     return hit
 
+
 def search_keywords(str):
     query = {
-        "query":{
-            "wildcard":{
-                "keywords": "*" + str + "*"
-            }
+        "query": {
+            "wildcard": {"keywords": "*" + str + "*"}
             # "match":{
             #     "keywords": str
             # }
-        }
-        ,"sort": [
-            {
-                "_score":{
-                    "order": "desc"
-                }
-            }
-        ]
-        ,"size":1000
+        },
+        "sort": [{"_score": {"order": "desc"}}],
+        "size": 1000,
     }
-    result = es.search(index="mydatabase",body=query)
+    result = es.search(index="mydatabase", body=query)
     hit = result["hits"]["hits"]
     return hit
 
@@ -159,7 +144,7 @@ def search_keywords(str):
 #                 "title.edgengram":str
 #             }
 #         },
-#         "size":1000        
+#         "size":1000
 #     }
 #     res = es.search(index="mydatabase",body=query,filter_path = ['**.hits'])
 #     json_print(res)
@@ -169,7 +154,7 @@ def search_keywords(str):
 #         "suggest": {
 #             "title_suggest": {
 #                 "text": str,
-#                 "completion": {    
+#                 "completion": {
 #                     "field": "title",
 #                     "skip_duplicates": True,
 #                     "size": 10
