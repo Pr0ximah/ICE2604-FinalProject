0ac01e615e601895c6b405aa78264e243739471c
jinjunshan <2987063134@qq.com>
2023-12-15 15:43:17 +0800

    登录状态验证，收藏功能

--- api/api_port/api_post.py
+++ api/api_port/api_post.py
@@ -8,6 +8,24 @@ from api_port.db_tool import sql_tool
 import hashlib
 from datetime import datetime
 
+class login_item(BaseModel):
+    user: str = ""
+    key: str = ""
+
+class login_time(BaseModel):
+    user: str = ""
+
+class confirm_password(BaseModel):
+    user: str = ""
+    password : str = ""
+
+class name_paperid(BaseModel):
+    user : str =""
+    paper_id : str =""
+
+class user_name(BaseModel):
+    user : str =""
+
 app_post = APIRouter()
 
 def generate_sha256_hash(input_string):
@@ -17,21 +35,25 @@ def generate_sha256_hash(input_string):
     return hashed_value
 
 @app_post.post("/signup")
-async def Signup(user :str, key :str):
-    datadepot = sql_tool("shan", "finalproject", "users")
+async def Signup(item: login_item):
+    user = item.user
+    key = item.key
+    datadepot = sql_tool("ADMINROOT", "ice2604_final_project", "users")
     content = datadepot.fetch_specific("user_name", user)
     if content: return False
     successsignin = False
     key_db=generate_sha256_hash(key)
-    datadepot.insert([f"{user}", f"{key_db}"])
+    datadepot.insert([f"{user}", f"{key_db}", None])
     datadepot.save()
     content = datadepot.fetch_specific("user_name", user)
     if content: successsignin = True
     return successsignin
 
 @app_post.post("/login")
-async def Login(user :str, key :str):
-    datadepot = sql_tool("shan", "finalproject", "users")
+async def Login(item: login_item):
+    user = item.user
+    key = item.key
+    datadepot = sql_tool("ADMINROOT", "ice2604_final_project", "users")
     content = datadepot.fetch_specific("user_name", user)
     successlogin = False
     if not content: return False
@@ -42,10 +64,12 @@ async def Login(user :str, key :str):
 
 # 登录的接口，返回password
 @app_post.post("/login_password")
-async def LoginPassword(user:str, time: datetime):
+async def LoginPassword(item : login_time):
     datadepot = sql_tool("shan", "finalproject", "users")
+    user = item.user
     content = datadepot.fetch_specific("user_name", user)
     successsigninpassword = False
+    time = datetime.now()
     time_str = time.strftime("%Y-%m-%d %H:%M:%S")
     password=user+time_str
     key_db=generate_sha256_hash(password)
@@ -58,27 +82,90 @@ async def LoginPassword(user:str, time: datetime):
 
 # 验证的接口，返回True和False
 @app_post.post("/confirm_password")
-async def Confiem_Password(user : str, password : str):
+async def Confiem_Password(item : confirm_password):
+    user = item.user
+    password = item.password
     datadepot = sql_tool("shan", "finalproject", "users")
     content = datadepot.fetch_specific("user_name", user)
     password_db = content[0][3]
     if password == password_db: return True
     else: return False
 
-# 收集能力没写完
 @app_post.post("/collectpaper")
-async def Collect(user: str, paper_id :str):
+async def Collect(item : name_paperid):
+    user = item.user
+    paper_id = item.paper_id
     datadepot = sql_tool("shan", "finalproject", "users")
-    datadepot_pdf = sql_tool("shan", "finalproject", "users")
-    content = datadepot_pdf.fetch_specific("paper_id", paper_id)  #在储存pdf的数据库中读取
-    '''根据content进行更改'''
-    title = content
-    author = content
-
-    content_user = datadepot.fetch_specific("user_name", user)
-    oldpaper = content_user[0][2]
+    content = datadepot.fetch_specific("user_name", user)
+    oldpaper = content[0][2]
+    # return oldpaper
+    paper_list = []
+    if not oldpaper:
+        paper_list.append(paper_id)
+        newpaper_dict = {"user" : paper_list}
+        newpaper = json.dumps(newpaper_dict)
+    else:
+        # print(oldpaper)
+        oldpaper_dict=json.loads(oldpaper)
+        paper_list = oldpaper_dict["user"]
+        for i in paper_list:
+            # print(i)
+            if (i == paper_id):
+                return {"error":"This article has been collected!"}
+        paper_list.append(paper_id)
+        newpaper_dict = {"user":paper_list}
+        newpaper= json.dumps(newpaper_dict)
+    datadepot.insert_column_by_username("love_paper", newpaper, user)
+    datadepot.save()
+    content = datadepot.fetch_specific("user_name", user)
+    content = json.loads(content[0][2])
+    return True
+    
+@app_post.post("/removePaper")
+async def RemovePaper(item : name_paperid):
+    user = item.user
+    paper_id = item.paper_id
+    datadepot = sql_tool("shan", "finalproject", "users")
+    content = datadepot.fetch_specific("user_name", user)
+    oldpaper = content[0][2]
+    if not oldpaper:
+        return {"error":"This user hasn't collect article!"}
     oldpaper_dict=json.loads(oldpaper)
-    oldpaper_dict[f"{paper_id}"] = [f"{title}", f"{author}"]
-    newpaper= json.dumps(oldpaper_dict)
-    datadepot.insert_column()
-    return content
+    paper_list = oldpaper_dict["user"]
+    if paper_id in paper_list:
+        paper_list.remove(paper_id)
+    else:
+        return {"error":"This is article is not collected by this user!"}
+    newpaper_dict = {"user":paper_list}
+    newpaper= json.dumps(newpaper_dict)
+    datadepot.insert_column_by_username("love_paper", newpaper, user)
+    datadepot.save()
+    return True
+
+@app_post.post("/get_collected_paper")
+async def RemoveCollectPaper(item : user_name):
+    user = item.user
+    datadepot = sql_tool("shan", "finalproject", "users")
+    datadepot_pdf = sql_tool("shan", "finalproject", "100_pdf_metadata")
+    content_paper = datadepot.fetch_specific("user_name", user)
+    content_paper_list_json = content_paper[0][2]
+    if not content_paper_list_json:
+        return {"error":"This user hasn't collect article!"}
+    paper_dict = json.loads(content_paper_list_json)
+    paper_list = paper_dict["user"]
+    if not paper_list:
+        return {"error":"This user hasn't collect article!"}
+    return_dict = {}
+    for paper_id in paper_list:
+        content_pdf = datadepot_pdf.fetch_specific("paper_id", paper_id)  #在储存pdf的数据库中读取
+        # return(content_pdf)
+        content_list = content_pdf[0]
+        return_list={}
+        return_list["DOI"] = content_list[18]
+        return_list["Year"] = content_list[4]
+        return_list["Jurnal"] = content_list[14]
+        return_list["Authors"] = content_list[6]
+        return_list["Keywords"] = content_list[3]
+        return_list["Abstract"] = content_list[9]
+        return_dict[f"{paper_id}"] = return_list
+    return return_dict
